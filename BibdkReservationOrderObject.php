cc1bdbcc2aadb969c029dfcf5c702cc0c2c5f3c3
Søren Vibjerg <vibjerg@gmail.com>
2012-09-13 11:43:33 +0200

    - Orderobject updated - Step three updated to validate against Borchk

--- BibdkReservationOrderObject.php
+++ BibdkReservationOrderObject.php
@@ -3,20 +3,44 @@
 class BibdkReservationOrderObject {
   private $agencyId;
   private $manifestationIds;
+  private $userData;
   
   public function setManifestationIds(array $ids){
     $this->manifestationIds = $ids;
   }
   
+  public function getManifestationIds(array $ids){
+    return $this->manifestationIds;
+  }
+  
   public function setAgencyId($id){
-    $this->agencyId = $id;
+    $this->agencyId = "DK-".$id;
   }
   
   public function getAgencyId(){
     return $this->agencyId;
   }
   
-  public function save(){
-    $_SESSION['orderobject'] = $this;
+  public function setUserData($userData){
+    $this->userData = $userData;
+  }
+
+  public function getUserData(){
+    return $this->userData;
+  }
+   
+  /**
+   * @return BibdkReservationOrderObject
+   */
+  public static function GetObject(){
+    return $_SESSION['orderobject'];
+  }
+  
+  /**
+   * @return BibdkReservationOrderObject
+   */
+  public static function SetObject(){
+    $_SESSION['orderobject'] = new BibdkReservationOrderObject();
+    return $_SESSION['orderobject'];
   }
 }
