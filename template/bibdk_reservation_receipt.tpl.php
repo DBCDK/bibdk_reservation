9f221b59c7c890d0e402b61374449f2351dc5735
martin møller <mmj@dbc.dk>
2012-09-19 16:00:15 +0200

    Fixes

--- template/bibdk_reservation_receipt.tpl.php
+++ template/bibdk_reservation_receipt.tpl.php
@@ -1,17 +1,15 @@
 <div class="receipt-container">
-  <h4>Ordre:</h4>
-  <p><?php t('Your ordernumber: ');
-print $variables['orderId']; ?></p>
-  <h3>Personlige oplysninger</h3>
+  <h4><?php print t('Your order was successfully completed:'); ?></h4>
+  <p><?php print t('Your order ID: ') . $variables['orderId']; ?></p>
+  <h3><?php print t('Personal information'); ?></h3>
   <?php $user = $variables['user']; ?>
   <?php foreach ($user as $key => $value) : ?>
     <?php print $key . ': ' . $value . '<br />'; ?>
 <?php endforeach; ?>
   <br />
-  <h4>Du bestiller:</h4>
+  <h4><?php print t('You are ordering: '); ?></h4>
   <p><?php print $variables['manifestation']; ?></p>
-  <p><?php print $variables['needBeforeDate']; ?></p>
-  <h3>Bibliotek</h3>
+  <h3><?php print t('Library'); ?></h3>
     <?php $branch = $variables['branch']; ?>
   <p><?php print $branch->branchName; ?><br/>
     <?php if (isset($branch->postalAddress) && $branch->postalAddress) : ?>
@@ -19,7 +17,7 @@ print $variables['orderId']; ?></p>
       <?php print $branch->postalCode; ?> <?php print $branch->city; ?><br/>
     <?php endif; ?>
     <?php if (isset($branch->openingHours) && $branch->openingHours) : ?>
-    <p>Åbningstider:<br/>
+    <p><?php print t('Opening hours'); ?><br/>
       <?php foreach ($branch->openingHours as $hours) : ?>
         <?php print $hours->{'$'}; ?>
         <br/>
