61f1dd9de920fa025d0e46be314100b40f618e1d
Martin Møller <mmj@dbc.dk>
2013-01-16 17:02:23 +0100

    "Pick another library"-button added.

--- template/bibdk_reservation_agency.tpl.php
+++ template/bibdk_reservation_agency.tpl.php
@@ -1,7 +1,7 @@
 <div class="bottom-space">
-  <b><?php print $name; ?></b><br/>
+    <b><?php print $name; ?></b><br/>
   <?php if (isset($address)) : ?>
-    <?php print $address; ?><br/>
-    <?php print $postalCode; ?> <?php print $city; ?><br/>
+  <?php print $address; ?><br/>
+  <?php print $postalCode; ?> <?php print $city; ?><br/>
   <?php endif; ?>
 </div>
