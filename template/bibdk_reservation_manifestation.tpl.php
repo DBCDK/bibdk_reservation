afd95b99f852e6099cabfa3ce62696ef4dbb0d28
Martin Møller <mmj@dbc.dk>
2013-01-16 13:50:29 +0100

    Reservation step 2 styles changed

--- template/bibdk_reservation_manifestation.tpl.php
+++ template/bibdk_reservation_manifestation.tpl.php
@@ -1,7 +1,7 @@
-<div class="bottom-space">
-<b><?php print $title; ?></b><br/>
-    <?php if (isset($author)) : ?>
-      <?php print $author; ?><br/>
-<?php endif; ?>    
-    <?php print $type; ?>
-</div>    
+<div>
+    <b><?php print $title; ?></b><br/>
+  <?php if (isset($author)) : ?>
+  <?php print $author; ?><br/>
+  <?php endif; ?>
+  <?php print $type; ?>
+</div>
