981437b93291e1c1482b442c1d2766be8e5d8c91
pjohans <pjohans@gmail.com>
2013-01-18 07:22:35 +0100

    css class on branch data

--- template/bibdk_reservation_agency.tpl.php
+++ template/bibdk_reservation_agency.tpl.php
@@ -1,7 +1,8 @@
-<div class="bottom-space">
-    <b><?php print $name; ?></b><br/>
+
+<div class="bottom-space branch-data">
+  <b><?php print $name; ?></b><br/>
   <?php if (isset($address)) : ?>
-  <?php print $address; ?><br/>
-  <?php print $postalCode; ?> <?php print $city; ?><br/>
+    <?php print $address; ?><br/>
+    <?php print $postalCode; ?> <?php print $city; ?><br/>
   <?php endif; ?>
 </div>
