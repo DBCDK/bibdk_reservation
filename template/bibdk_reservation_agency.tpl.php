d297ee8792c4d68e3987ddb368949cd41bd4a6fd
Anders-Andersen <ana@dringo.dbc.dk>
2013-07-19 12:37:10 +0200

    949_branchShortName

--- template/bibdk_reservation_agency.tpl.php
+++ template/bibdk_reservation_agency.tpl.php
@@ -1,6 +1,9 @@
 
 <div class="bottom-space branch-data">
   <b><?php print $name; ?></b><br/>
+  <?php if (isset($shortname)) : ?>
+    <?php print $shortname; ?><br/>
+  <?php endif; ?>
   <?php if (isset($address)) : ?>
     <?php print $address; ?><br/>
     <?php print $postalCode; ?> <?php print $city; ?><br/>
