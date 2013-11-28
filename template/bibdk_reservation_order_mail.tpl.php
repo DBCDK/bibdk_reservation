1c3d016a617130a3850f5ae2d6a93c6a85423b86
Anders-Andersen <ana@dringo.dbc.dk>
2013-11-28 19:01:24 +0100

    1116 review fix extra extra - excl automatic test

--- template/bibdk_reservation_order_mail.tpl.php
+++ template/bibdk_reservation_order_mail.tpl.php
@@ -1,7 +1,7 @@
 <div>
   <?php 
   if (isset($IllOrderReceiptText)) :
-      print $IllOrderReceiptText; print "\n\n"; 
+    print $IllOrderReceiptText; print "\n\n"; 
   endif;
   
   print $headertext; print "\n\n";
@@ -11,14 +11,13 @@
   print "\n\n"; print $userparameter; print "\n\n"; 
   
   foreach ($userinfo as $list) :
-      print $list; print "\n"; 
+    print $list; print "\n"; 
   endforeach; 
    
-  print "\n\n"; print $manifestation;
+  print "\n\n"; print $manifestation; print "\n\n";
   
-  print "\n"; 
   foreach ($title as $list) : 
-     print $list; print "\n"; 
+    print $list; print "\n"; 
   endforeach; 
   
   print "\n"; print $branch; print "\n"; 
@@ -28,10 +27,9 @@
   
   if (isset($orderinfo)) : 
     print "\n\n"; print $orderparameters;print "\n\n"; 
-      foreach ($orderinfo as $data) : 
-        print $data; print "\n"; 
-      endforeach; 
+    foreach ($orderinfo as $data) : 
+      print $data; print "\n"; 
+    endforeach; 
   endif; 
   ?>
-    
 </div>
