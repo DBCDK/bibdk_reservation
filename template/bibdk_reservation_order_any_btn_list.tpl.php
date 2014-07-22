74b861ffe2102a91125bff33d88dd07af3689098
Martin Møller <mmj@dbc.dk>
2014-07-22 12:21:54 +0200

    codeformatting

--- template/bibdk_reservation_order_any_btn_list.tpl.php
+++ template/bibdk_reservation_order_any_btn_list.tpl.php
@@ -1,9 +1,5 @@
-<?php
-  $items['title'] = '<span class="title">' . $items['title'] . '</span><span class="icon icon-right icon-blue-down"> </span>'
-?>
+<?php $items['title'] = '<span class="title">' . $items['title'] . '</span><span class="icon icon-right icon-blue-down"> </span>' ?>
 
 <div class="col<?php print $column; ?>">
-
   <?php print theme_item_list($items); ?>
-
 </div>
