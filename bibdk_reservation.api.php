99d3cc8ba21921652523b1360d5b4e03b8c91c3c
Martin Møller <mmj@dbc.dk>
2014-07-23 13:30:23 +0200

    Updated documentation of hook_order_any_btn_lists().

--- bibdk_reservation.api.php
+++ bibdk_reservation.api.php
@@ -25,43 +25,31 @@ function hook_bibdk_reservation_complete($pids, $result) {
 
 /**
  * Definition of hook_order_any_btn_lists().
- * Makes it possible for other modules to add/manipulate the contents of the
+ * Makes it possible for other modules to add content to the
  * 'Order any edition' button shown on each collection.
  *
- * In the basic example below another list is added to the button but any
- * content given in a render array could be added, as it will be rendered by the
- * drupal_render() method.
+ * Any content given in a render array could be added, as it will be rendered
+ * by the drupal_render() method.
+ * Note that the structure given in the example below should be used as the key
+ * ('header_of_the_column' in the example below) will be translated and used as
+ * header for the column.
  *
- * @param array $lists Array with the content of the 'Order any edition'
- *                            button so far collected.
+ * @param string $type    The material type of the material associated with the
+ *                        given $ids.
  *
- * @param array $collection Array with the collections that will be displayed.
- *                            Each collection will get its own 'Order any
- *                            edition' button.
+ * @param array $ids      Array with the $ids associated with the current
+ *                        material.
  *
- * @return array $lists       Array with the lists including the one your
- *                            implementation of the hook is providing.
- *
- * @see includes/common.inc - drupal_render();
+ * @return array $item    Render array of any kind.
+ *                        @see includes/common.inc - drupal_render();
  */
-//TODO mmj rewrite this to reflect latest changes
-function hook_order_any_btn_lists($lists, $collection) {
-  $item1 = l('my-link-1', 'my/path/1');
-  $item2 = l('my-link-2', 'my/path/2');
+function hook_order_any_btn_lists($type, $ids) {
 
-  $lists[] = array(
-    '#type' => 'ol',
-    '#theme' => 'item_list',
-    '#title' => 'My List',
-    '#items' => array($item1, $item2),
-    '#prefix' => '<div>',
-    '#suffix' => '</div>',
-    '#attributes' => array(
-      'class' => array(
-        'my-class',
-      ),
-    ),
+  $item = array(
+    'header_of_the_column' => array(
+      '#theme' => 'any_theme_you_would_like_to_use',
+    )
   );
 
-  return $lists;
+  return $item;
 }
