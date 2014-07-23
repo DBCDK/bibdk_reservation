5b92813190cedfda0c9e08b9652ec68f1883d60b
Martin Møller <mmj@dbc.dk>
2014-07-23 13:16:38 +0200

    Removed unused templates

--- template/bibdk_reservation_order_any_btn_list.tpl.php
+++
@@ -1,7 +0,0 @@
-<!-- TODO mmj possibly unused template after 1394 - remove!-->
-<?php $items['title'] = '<span class="title">' . $items['title'] . '</span><span class="icon icon-right icon-blue-down"> </span>' ?>
-
-<!-- TODO mmj remember to remove deprecated classes -->
-<div class="col<?php print $column; ?>">
-  <?php print theme_item_list($items); ?>
-</div>
