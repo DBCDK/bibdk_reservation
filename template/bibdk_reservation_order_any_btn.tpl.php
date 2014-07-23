d418576e4742b195bfb1a400117816f1dcc10f44
Martin Møller <mmj@dbc.dk>
2014-07-23 15:21:41 +0200

    comitting for features build - visuallyhidden outcommented

--- template/bibdk_reservation_order_any_btn.tpl.php
+++ template/bibdk_reservation_order_any_btn.tpl.php
@@ -2,7 +2,7 @@
   <div class="primary-actions">
     <div class="dropdown-wrapper">
       <a <?php print drupal_attributes($addi_attributes);?> class="btn btn-blue dropdown-toggle toggled" href="#"><?php print t('Order any edition', array(), array('context'=>'bibdk_reservation')); ?> <span class="icon icon-right icon-white-down">&nbsp;</span></a>
-      <div class="dropdown-menu order-any-btn-list">
+      <div class="dropdown-menu order-any-btn-list"> <!-- visuallyhidden -->
         <?php print $table; ?>
       </div>
     </div>
