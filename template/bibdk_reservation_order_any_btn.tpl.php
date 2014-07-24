ce4a8836bac763fe7b9074ecf9b2f36afc329e85
Martin Møller <mmj@dbc.dk>
2014-07-24 10:48:10 +0200

    Removed develop features

--- template/bibdk_reservation_order_any_btn.tpl.php
+++ template/bibdk_reservation_order_any_btn.tpl.php
@@ -1,8 +1,8 @@
 <div class="actions">
   <div class="primary-actions">
     <div class="dropdown-wrapper">
-      <a <?php print drupal_attributes($addi_attributes);?> class="btn btn-blue dropdown-toggle toggled" href="#"><?php print t('Order any edition', array(), array('context'=>'bibdk_reservation')); ?> <span class="icon icon-right icon-white-down">&nbsp;</span></a>
-      <div class="dropdown-menu order-any-btn-list"> <!-- visuallyhidden -->
+      <a <?php print drupal_attributes($addi_attributes);?> class="btn btn-blue dropdown-toggle" href="#"><?php print t('Order any edition', array(), array('context'=>'bibdk_reservation')); ?> <span class="icon icon-right icon-white-down">&nbsp;</span></a>
+      <div class="dropdown-menu order-any-btn-list visuallyhidden">
         <?php print $table; ?>
       </div>
     </div>
